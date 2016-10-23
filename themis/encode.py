7230ad12ce9db518ee882c96288c3d3456bef1ba
Martastain <martas@imm.cz>
2016-10-23 22:29:37 +0200

    refactored

---
+++ themis/encode.py
@@ -0,0 +1,104 @@
+import re
+import os
+import subprocess
+
+from nxtools import *
+from .sox import Sox
+from .output_profile import get_output_profile
+
+__all__ = ["encode"]
+
+def encode(parent):
+    source_fps = parent.meta["frame_rate"]
+    profile_fps = parent.settings["frame_rate"]
+    sox_tempo = float(profile_fps) / source_fps
+
+    if source_fps >= profile_fps or profile_fps - source_fps > 3:
+        encode_method = "direct"
+        input_format = []
+        output_format = [
+                ]
+        track_mapping = [["map", "0:{}".format(parent.meta["video_index"])]]
+    else:
+        encode_method = "reclock"
+        source_format = [
+                    ["an"],
+                    ["map", "0:{}".format(parent.meta["video_index"])],
+                    ["filter:v", parent.filters],
+                    ["pix_fmt", parent.settings["pixel_format"]],
+                    ["f", "rawvideo"],
+                ]
+        input_format = [
+                    ["f", "rawvideo"],
+                    ["pix_fmt", parent.settings["pixel_format"]],
+                    ["s", "{}x{}".format(parent.settings["width"], parent.settings["height"])],
+                ]
+        output_format = []
+        track_mapping = [["map", "0:0"]]
+
+
+
+    for i, track in enumerate(parent.audio_tracks):
+        parent.set_status("Stretching audio track {} of {}".format(i+1, len(parent.audio_tracks)))
+        if encode_method == "reclock":
+            logging.debug("Reclocking audio by factor {}".format(sox_tempo))
+            f_in = track.source_audio_path
+            f_out = track.final_audio_path = get_temp("wav")
+            cmd = [
+                    f_in,
+                    "-r", parent.settings.get("audio_sample_rate", 48000),
+                    f_out
+                ]
+            cmd.extend(["tempo", sox_tempo])
+            sox = Sox(*cmd)
+            result = sox.start(handler=parent.progress_handler)
+
+        track_mapping.append(["map", "{}:{}".format(i+1, 0)])
+        output_format.append(["i", track.final_audio_path])
+
+
+    parent.set_status("Transcoding")
+
+    output_format.extend(track_mapping)
+
+    if encode_method == "direct":
+        output_format.append(["filter:v", parent.filters])
+
+    output_format.extend(get_output_profile(**parent.settings))
+
+    if encode_method == "reclock":
+        dec = FFMPEG(parent.source_path, "-", source_format)
+        dec.start(stdout=subprocess.PIPE)
+        enc_input = "-"
+        enc_stdin = dec.stdout
+        enc_stderr = open(os.devnull, "w")
+    else:
+        enc_input = parent.source_path
+        enc_stdin = None
+        enc_stderr = subprocess.PIPE
+        enc_stderr = None
+
+
+    enc = FFMPEG(enc_input, parent.output_path, output_format, input_format)
+    enc.start(stdin=enc_stdin, stderr=enc_stderr)
+
+    if encode_method == "reclock":
+        dec.stdout.close()
+        while dec.is_running or enc.is_running:
+            if dec.is_running:
+                dec.process(progress_handler=lambda x: parent.progress_handler(float(x) / parent.meta["num_frames"] * 100))
+            if enc.is_running:
+                enc.process()
+
+        if dec.return_code or enc.return_code:
+                logging.error("Decoding failed with following error:\n\n{}\n\n".format(indent(dec.error_log)))
+                logging.error("Encoding failed with following error:\n\n{}\n\n".format(indent(enc.error_log)))
+
+    else:
+        while enc.is_running:
+            enc.process(progress_handler=lambda x: parent.progress_handler(float(x) / parent.meta["num_frames"] * 100))
+
+
+
+
+
--- themis/themis.py
+++ themis/themis.py
@@ -1,114 +1,64 @@
-# TODO
-#   - Reimplement sox
-#   - Reporting (return meta dict)
-#   - Reclock encoder error handling
-#   - Move matching files
-
-from __future__ import print_function
-
 import os
-import sys
 import time
 
 from nxtools import *
 
-# Trancoding methods
-
-from .reclock import *
-from .audio import *
-from .direct import *
+from .output_profile import *
+from .probe import *
+from .extract import extract
+from .encode  import encode
 
 __all__ = ["Themis"]
 
+default_settings = {
+        "container" : "mov",
+        "output_dir" : "output",
 
-class AudioTrack():
-    def __init__(self, **kwargs):
-        self.data = kwargs
+        "width" : 1920,
+        "height" : 1080,
+        "frame_rate" : 25,
+        "pixel_format" : "yuv422p",
+        "video_codec" : "dnxhd",
+
+        # optional
+
+        "video_bitrate" : False,
+        "gop_size"      : False,
+        "audio_codec"   : False,
+        "audio_bitrate" : False,
+
+        #x264/x265 settings
+        "level"         : False,
+        "preset"        : False,
+        "profile"       : False,
+
+        # Helpers
+        "expand_levels" : False, # Expand tv color levels to full
+        "deinterlace"   : True,  # Enable smart deinterlace (slower)
+        "crop_detect"   : False,  # Enable smart crop detection (slower)
+        "loudness"      : -23,
+        "logo"          : False,
+
+        "strip_tracks"   : 2,    # 0 - keep all audio channels, 1 - Keep only first track, 2 - Keep only first track or keep all if they are mono
+        "to_stereo"      : True, # Mixdown multichannel audio tracks to stereo
+    }
+
+
+class Themis(object):
+    def __init__(self, source_path, **kwargs):
+        self.source_path = source_path
+        self.settings = default_settings
+        self.settings.update(kwargs)
+        logging.debug("Probing {}".format(self.friendly_name))
+        self.meta = probe(source_path)
+        self.last_progress_time = time.time()
+        self.progress = 0
+        self.status = "Pending ingest {}".format(self.friendly_name)
+
 
     def __getitem__(self, key):
-        return self.data[key]
+        return self.settings[key]
 
-    @property
-    def id(self):
-        return self["index"]
-
-
-class Themis():
-    def __init__(self, input_path, **kwargs):
-        self.input_path = input_path
-        self.settings = self.defaults
-        self.settings.update(kwargs)
-        self.temp_files = []
-        self.filters = []
-        self.probe_result = {}
-        self.analyse_result = {}
-        self.meta = {
-                "container" : os.path.splitext(self.input_path)[1].lstrip(".")
-                }
-        self.status = "Starting conversion"
-
-    @property
-    def defaults(self):
-        settings = {
-            "frame_rate" : 25,
-            "loudness" : -23.0,
-            "deinterlace" : True,
-            "container" : "mov",
-            "width" : 1920,
-            "height" : 1080,
-            "pixel_format" : "yuv422p",
-            "video_codec" : "dnxhd",
-            "video_bitrate" : "36M",
-            "audio_codec" : "pcm_s16le",
-            "audio_sample_rate" : 48000
-            }
-        return settings
-
-    ##
-    # Temporary files handling
-    ##
-
-    def get_temp(self, extension=False):
-        filename = get_temp(extension)
-        self.temp_files.append(filename)
-        return filename
-
-    def clean_up(self):
-        if self.temp_files:
-            logging.debug("Cleaning-up temporary files")
-        for f in self.temp_files:
-            if not os.path.exists(f):
-                continue
-            try:
-                os.remove(f)
-            except:
-                logging.warning("Unable to remove temporary file {}".format(f))
-
-    def __del__(self):
-        self.clean_up()
-
-    ##
-    # Paths and names
-    ##
-
-    @property
-    def base_name(self):
-        return self.settings.get("base_name", False) or get_base_name(self.input_path)
-
-    @property
-    def friendly_name(self):
-        return self.settings.get("friendly_name", False) or self.base_name
-
-    @property
-    def output_path(self):
-        return self.settings.get("output_path", False) or  os.path.join(
-                self.settings["output_dir"],
-                "{}.{}".format(self.base_name, self.settings["container"])
-                )
-
-    ##
-    # Logging
-    ##
 
     def set_status(self, message, level="debug"):
         self.status = message
@@ -121,178 +71,104 @@ class Themis():
             "good_news" : logging.goodnews
         }.get(level, False)(message)
 
+
+    @property
+    def base_name(self):
+        return self.settings.get(
+                "base_name",
+                get_base_name(self.source_path)
+            )
+
+
+    @property
+    def friendly_name(self):
+        return self.settings.get("friendly_name", False) or self.base_name
+
+
+    @property
+    def output_path(self):
+        if "output_path" in self.settings:
+            return self.settings["output_path"]
+        if "output_dir" in self.settings:
+            return os.path.join(
+                self.settings["output_dir"], "{}.{}".format(
+                        self.settings["output_dir"],
+                        self.settings["container"]
+                    )
+                )
+
+
+    @property
+    def audio_tracks(self):
+        return self.meta.get("audio_tracks", [])
+
+
+    @property
+    def strip_tracks(self):
+        if not self["strip_tracks"]:
+            return False
+        strip_tracks = self["strip_tracks"]
+        if list(set([ track["channels"] for track in self.audio_tracks])) == 1:
+            if strip_tracks == 2 and self.audio_tracks[0]["channels"] == 1:
+                return False
+        return strip_tracks
+
+
+    @property
+    def filters(self):
+        filters = []
+        if self.settings["deinterlace"] and self.meta["is_interlaced"]:
+            logging.debug("Video will be deinterlaced")
+            filters.append(filter_deinterlace())
+
+        filters.append(
+            filter_arc(self.settings["width"],
+                self.settings["height"],
+                self.meta["aspect_ratio"]
+                )
+            )
+        return join_filters(*filters)
+
+
     def progress_handler(self, progress):
-        pass
+        if time.time() - self.last_progress_time > 3:
+            logging.debug("{} {} ({:.02f}% done)".format(
+                    self.status,
+                    self.friendly_name,
+                    progress
+                ))
+            self.last_progress_time = time.time()
 
-    def frame_progress(self, at_frame):
-        progress = min(99.99, (at_frame / self.num_frames) * 100)
-        self.progress_handler(progress)
-
-    ##
-    # Source metadata extraction
-    ##
-
-    def analyse(self, video=True, audio=True):
-        self.current_phase = 1
-        self.set_status("Analysing {}".format(self.friendly_name))
-        self.analyse_result = ffanalyse(self.input_path, progress_handler=self.frame_progress, video=video, audio=audio)
-
-    def probe(self):
-        self.set_status("Probing {}".format(self.friendly_name))
-        self.probe_result = ffprobe(self.input_path)
-        self.audio_tracks = []
-
-        format_info = self.probe_result["format"]
-
-        for stream in self.probe_result["streams"]:
-            if stream["codec_type"] == "video":
-                # Frame rate detection
-                fps_n, fps_d = [float(e) for e in stream["r_frame_rate"].split("/")]
-                self.meta["frame_rate"] = fps_n / fps_d
-
-                # Aspect ratio detection
-                try:
-                    dar_n, dar_d = [float(e) for e in stream["display_aspect_ratio"].split(":")]
-                    if not (dar_n and dar_d):
-                        raise Exception
-                except:
-                    dar_n, dar_d = float(stream["width"]), float(stream["height"])
-                self.meta["aspect_ratio"] = dar_n / dar_d
-
-                # Video track duration
-                try:
-                    source_vdur = float(stream["duration"])
-                except:
-                    source_vdur = False
-
-                self.meta["video_codec"] = stream["codec_name"]
-                self.meta["pixel_format"] = stream["pix_fmt"]
-                self.meta["width"] = stream["width"]
-                self.meta["height"] = stream["height"]
-                self.meta["video_index"] = stream["index"]
-
-            elif stream["codec_type"] == "audio":
-                self.audio_tracks.append(AudioTrack(**stream))
-
-        self.meta["duration"] = float(format_info["duration"]) or source_vdur
-        self.meta["num_frames"] = self.meta["duration"] * self.meta["frame_rate"]
-
-
-    @property
-    def loudness(self):
-        key = "audio/r128/i"
-        if not key in self.analyse_result:
-            self.analyse(video=False)
-        return self.analyse_result.get(key, self.settings["loudness"])
-
-    @property
-    def is_interlaced(self):
-        key = "video/is_interlaced"
-        if not key in self.analyse_result:
-            self.analyse()
-        return self.analyse_result.get(key, False)
-
-    @property
-    def gain_change(self):
-        gain_change = self.settings.get("loudness", -23) - self.loudness
-        return gain_change if (self.loudness and abs(gain_change) > 0.5) else 0
-
-    @property
-    def duration(self):
-        return self.meta["duration"]
-
-    @property
-    def num_frames(self):
-        return self.meta["num_frames"]
-
-    ##
-    # Main process
-    ##
 
     def process(self, **kwargs):
-        start_time = time.time()
-        logging.debug("Updating settings:", str(kwargs))
+        """v1 compatibility"""
+        self.start(**kwargs)
+
+
+    def start(self, **kwargs):
         self.settings.update(kwargs)
-        self.set_status("Transcoding {}".format(self.friendly_name), level="info")
-        try:
-            result = self._process()
-        except KeyboardInterrupt:
-            print ()
-            logging.warning("Transcoding aborted", level="warning")
-            result = False
-        if not result:
-            self.fail_clean_up()
-            self.set_status("Transcoding failed", level="error")
-            return False
+        start_time = time.time()
+
+        logging.debug("There are {} audio tracks".format(len(self.audio_tracks)))
+        if self.audio_tracks and self.strip_tracks:
+            logging.debug("Strip audio tracks")
+            self.meta["audio_tracks"] = [self.audio_tracks[0]]
+
+        self.set_status("Extracting tracks")
+        self.meta.update(extract(self))
+
+        encode(self)
+
+        total_duration = self.meta["num_frames"] / self.meta["frame_rate"]
         end_time = time.time()
         proc_time = end_time - start_time
-        speed = self.duration / proc_time
+        speed = total_duration / proc_time
         self.set_status(
             "Transcoding {:.2f}s long video finished in {} ({:.2f}x realtime)".format(
-                self.duration,
+                total_duration,
                 s2words(proc_time),
                 speed
                 ),
             level="good_news"
             )
         return True
-
-    def fail_clean_up(self):
-        if os.path.exists(self.output_path):
-            os.remove(self.output_path)
-
-    def _process(self):
-        if not self.probe_result:
-            self.probe()
-        ##
-        # Check, which streams must be re-encoded
-        ##
-
-        compare_v = [
-            "container",
-            "frame_rate",
-            "video_codec",
-            "pixel_format",
-            "width",
-            "height",
-            ]
-
-        for key in compare_v:
-            if self.settings[key] != self.meta[key]:
-                logging.debug(
-                    "Source {} does not match target format. IS: {} SHOULD BE: {}".format(
-                        key,
-                        self.meta[key],
-                        self.settings[key]
-                        )
-                    )
-                encode_video = True
-                break
-        else:
-            encode_video = False
-
-        ##
-        # Decide, which method will be used and execute!
-        ##
-
-        if encode_video:
-            # Create video filter chain
-            if self.settings["deinterlace"] and self.is_interlaced:
-                logging.debug("Video will be deinterlaced")
-                self.filters.append(filter_deinterlace())
-            self.filters.append(
-                filter_arc(self.settings["width"],
-                    self.settings["height"],
-                    self.meta["aspect_ratio"]
-                    )
-                )
-
-            source_fps = self.meta.get("frame_rate", 25)
-            profile_fps = self.settings.get("frame_rate", 25)
-            if source_fps >= profile_fps or profile_fps - source_fps > 3:
-                encode_method = encode_direct
-            else:
-                encode_method = encode_reclock
-        else:
-            encode_method =  encode_audio_only
-        return encode_method(self)
